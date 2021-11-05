769fdd5c30c7f7c80acfebb840fb78c969a544f1
Remon_Hany <remonhany2000@hotmail.com>
2021-11-06 00:27:07 +0200

    testing

--- REQUIREMENTS.md
+++ REQUIREMENTS.md
@@ -10,14 +10,14 @@
 
 - [+] Have a minimum of one form field
 
-- [ ] Make one request to the Aylien API
+- [+] Make one request to the Aylien API
 
 - [+] Use Sass for styling
 
 - [+] Minify js and styles in the production environment
 
-- [ ] Response from the API must be added to the view for a user to see 
+- [+] Response from the API must be added to the view for a user to see 
 
-- [ ] Be able to show content offline
+- [+] Be able to show content offline
 
 All of these requirements are explained throughout the course. Specifics on setting up the Aylien API are found in the README file of this repo.
--- src/__test__/nameChecker.test.js
+++ src/__test__/nameChecker.test.js
@@ -1,42 +1,60 @@
+import { when } from 'jest-when';
 import { nameChecker } from '../client/js/nameChecker'
 
 describe("Testing the Check Name functionality", () => {
     // The test() function has two arguments - a string description, and an actual test as a callback function.  
     test("Testing the checkForName() function", () => {
-        // Define the input for the function, if any, in the form of variables/array
-        //    let event;
-        let names = [
-            "Picard",
-            "Janeway",
-            "Kirk",
-            "Archer",
-            "Georgiou"
+        let urls = [
+            'http://foo.com/blah_blah',
+            'http://foo.com/blah_blah/',
+            'http://foo.com/blah_blah_(wikipedia)',
+            'http://foo.com/blah_blah_(wikipedia)_(again)',
+            'http://www.example.com/wpstyle/?p=364',
+            'https://www.example.com/foo/?bar=baz&inga=42&quux',
         ]
-        // Define the expected output, if any, in the form of variables/array
+        let invalidurls = [
+            'http://foo.bar?q=Spaces should be encoded',
+            '//',
+            '//a',
+            '///a',
+            '///',
+            'http:///a',
+            'foo.com',
+            'rdar://1234',
+            'h://test'
+        ]
+        const fn = jest.fn();
+        urls.forEach(element => {
+            when(fn).calledWith(element).mockReturnValue(true);
+
+            // expect(nameChecker(element)).toBeTruthy()
+        });
+
+        urls.forEach(element => {
+            when(fn).calledWith(element).mockReturnValue(false);
+        });
+        // invalidurls.forEach(element => {
+        //     expect(!nameChecker(element)).toBeTruthy()
+        // });
         
-        // const TestModule = (function() {
-            //     const notification = () => {
-                //        window.alert("Welcome, Captain!")
-                //     };
-                //     return {
-                    //        notification: notification,
-                    //     };
-        //  })();
-        expect(handleSubmit).toBeUndefined();
-        const mockCallback = jest.fn()
-        forEach(names, mockCallback);
-        expect(mockCallback.mock.results[0].value).toBeCalled();
-        // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
-        // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
-        // for (let i=0; i<names.length;i++) {
-        //     // jest.spyOn(global, 'alert').mockImplementation(() => {});
-        //     // Object.defineProperty(global, "window", {
-        //     //     value: {
-        //     //        alert: jest.fn(),
-        //     //     },
-        //     //  });
-        //     nameChecker(names[i])
-        //     expect(global.alert).toBeCalled();
-        // }
+
+
+
+
+        // const mockCallback = jest.fn()
+        // forEach(names, mockCallback);
+        // expect(mockCallback.mock.results[0].value).toBeCalled();
+        // // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
+        // // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
+        // // for (let i=0; i<names.length;i++) {
+        // //     // jest.spyOn(global, 'alert').mockImplementation(() => {});
+        // //     // Object.defineProperty(global, "window", {
+        // //     //     value: {
+        // //     //        alert: jest.fn(),
+        // //     //     },
+        // //     //  });
+        // //     nameChecker(names[i])
+        // //     expect(global.alert).toBeCalled();
+        // // }
     })
 });
